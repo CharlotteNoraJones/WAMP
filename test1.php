918ddb6b8ee45ab85a8444f655658e1896e39ddf
CharlotteNoraJones <47127622+CharlotteNoraJones@users.noreply.github.com>
2023-03-21 15:49:07 -0500

    More test

--- test1.php
+++ test1.php
@@ -4,4 +4,15 @@
     echo "<br>";
     $current_user = $username;
     echo $current_user;
+    echo "<br>";
+
+    $author = "Brian W. Kernighan";
+
+    echo <<<_END
+        Debugging is twice as hard as writing the code in the first place. 
+        Therefore, if you write the code as cleverly as possible, you are,
+        by definition, not smart enough to debug it.
+
+        - $author;
+    _END; 
 ?>
